d8edf4a606300f0ce1a09b320a5d02b5cba8cc0c
hamzaMansoor1236 <hamza.mansoor1236@gmail.com>
2022-12-25 01:51:18 +0500

    comments added

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-
+//the function called on 
 app.get('/',(req,res)=>{
     res.send("Hello World ")
 })
 
 
+//app running at the port specified in port 
 app.listen(port,()=>{
     console.log(" App running at port http://localhost:5000/ ")
 })
